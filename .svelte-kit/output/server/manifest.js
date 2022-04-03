dbd318170bc1d902ced571c2de8489f68ae6ef4a
Michele Dorph De Chiffre <md@autonik.dk>
2022-04-03 08:40:35 +0200

    Aggiunge un pagina di fallback per usare sul PHP server
    Allora anche se navigi a /chords/canzone/992 mostra una pagina se ricarichi la pagina.

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -1,9 +1,9 @@
 export const manifest = {
 	appDir: "internal",
-	assets: new Set(["canzoni/Dance Monkey.pro","canzoni/Elefantens Vuggevise.pro","canzoni/Hallelujah Italiano.pro","canzoni/Julies Sprog - Melodi Benny Holst - Tekst Benny Holst.pro","canzoni/Kald det Kærlighed.pro","canzoni/Livstræet - Hans HolmErik Lindebjerg.pro","canzoni/Magi i luften.pro","canzoni/Ninna Nanna degli Animaletti.pro","canzoni/Papirsklip.pro","canzoni/Regnvejrsdag i November.pro","canzoni/Rolling in the Deep.pro","canzoni/Sig du kan li mig.pro","canzoni/Tusind stykker.pro","favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
+	assets: new Set([".htaccess","canzoni/Dance Monkey.pro","canzoni/Elefantens Vuggevise.pro","canzoni/Hallelujah Italiano.pro","canzoni/Julies Sprog - Melodi Benny Holst - Tekst Benny Holst.pro","canzoni/Kald det Kærlighed.pro","canzoni/Livstræet - Hans HolmErik Lindebjerg.pro","canzoni/Magi i luften.pro","canzoni/Ninna Nanna degli Animaletti.pro","canzoni/Papirsklip.pro","canzoni/Regnvejrsdag i November.pro","canzoni/Rolling in the Deep.pro","canzoni/Sig du kan li mig.pro","canzoni/Tusind stykker.pro","favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain",".webp":"image/webp"},
 	_: {
-		entry: {"file":"start-8001782f.js","js":["start-8001782f.js","chunks/index-5db440d4.js","chunks/index-2e263ff2.js"],"css":[]},
+		entry: {"file":"start-f3fa2666.js","js":["start-f3fa2666.js","chunks/index-5db440d4.js","chunks/index-2e263ff2.js"],"css":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
