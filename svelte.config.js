320361d9272bf7bfc949268b12a62b46bcce3a7d
Michele Dorph De Chiffre <md@autonik.dk>
2022-04-04 11:56:26 +0200

    Revert "Test to enable vercel next to static adapter"
    This reverts commit 2f9d881767646fca61b7c03795fc0d29f969e389.

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,5 @@
-import autoAdapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-
-const is_vercel = typeof process.env.IS_VERCEL !== "undefined" ? process.env.IS_VERCEL : false;
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,22 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: is_vercel ? autoAdapter() : adapter({
-			fallback: '200.html'
-		}),
-
-		paths: {
-			base: (dev && !is_vercel) ? '' : '/chords',
-		}, 
-
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		},
-
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'internal',
+		adapter: adapter(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
